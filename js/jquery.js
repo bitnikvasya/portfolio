0b6c51b5d3dce34a7eafe84ffabac0ba13c31deb
Vasily Yakovenko <bitnikvasya@mail.ru>
2020-04-08 18:48:52 +0300

    mobile

---
+++ js/jquery.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function($) {
+
+$(".contacts__form").submit(function() {
+var str = $(this).serialize();
+
+$.ajax({
+type: "POST",
+url: "https://formfor.site/send/DFRJPHUPfRgZ41Gc25T1bJ7HVFq503",
+data: str,
+success: function(msg) {
+if(msg == 'OK') {
+  
+} else {
+  
+}
+$(".contacts__form-success").addClass("contacts__form-success--show")
+}
+});
+return false;
+});
+});
+
+
+jQuery(function($) {
+
+    const section = $('.section'),
+          nav = $('.navigation'),
+          navHeight = nav.outerHeight(); // получаем высоту навигации 
+
+    // поворот экрана 
+    window.addEventListener('orientationchange', function () {
+        navHeight = nav.outerHeight();
+    }, false);
+
+    $(window).on('scroll', function () {
+        const position = $(this).scrollTop();
+
+        section.each(function () {
+            const top = $(this).offset().top - navHeight - 5,
+                  bottom = top + $(this).outerHeight();
+
+            if (position >= top && position <= bottom) {
+                nav.find('a').removeClass('active');
+                section.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+
+    nav.find('a').on('click', function () {
+        const id = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - navHeight
+        }, 500);
+
+        return false;
+    });
+
+});
--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ let sendForm = document.querySelector('.contacts__form-button');
 let formFields = document.querySelectorAll('.contacts__form-input');
 let successMessage = document.querySelector('.contacts__form-success');
 let successToggle = document.querySelector('.contacts__form-success-toggle');
+let upButton = document.querySelector('.up-button');
+let projectsItems = document.querySelectorAll('.projects__item');
+let projectsItemDescriptions = document.querySelectorAll('.projects__item-description');
 
 let navToggle = function(x, y, z) {
   y.onclick = function(a) {
@@ -17,10 +20,10 @@ let navToggle = function(x, y, z) {
   	y.classList.remove('navigation__toggle--open')
   	y.classList.remove('animate-left')
   	y.classList.add('navigation__toggle--close')
-  	y.classList.add('animate-right')
+  	y.classList.add('animate-right-toggle')
   } else {
   	y.classList.remove('navigation__toggle--close')
-  	y.classList.remove('animate-right')
+  	y.classList.remove('animate-right-toggle')
   	y.classList.add('navigation__toggle--open')
   	y.classList.add('animate-left')
   }
@@ -29,10 +32,10 @@ let navToggle = function(x, y, z) {
   	x.classList.remove('navigation__list--closed')
   	x.classList.remove('animate-left')
   	x.classList.add('navigation__list--opened')
-  	x.classList.add('animate-right')
+  	x.classList.add('animate-right-navigation')
   } else {
   	x.classList.remove('navigation__list--opened')
-  	x.classList.remove('animate-right')
+  	x.classList.remove('animate-right-navigation')
   	x.classList.add('navigation__list--closed')
   	x.classList.add('animate-left')
   }
@@ -41,59 +44,34 @@ let navToggle = function(x, y, z) {
 
 navToggle(navigationMain, navigationToggle, header);
 
-jQuery(document).ready(function($) {
-
-$(".contacts__form").submit(function() {
-var str = $(this).serialize();
-
-$.ajax({
-type: "POST",
-url: "https://formfor.site/send/DFRJPHUPfRgZ41Gc25T1bJ7HVFq503",
-data: str,
-success: function(msg) {
-if(msg == 'OK') {
-  
-} else {
-  
-}
-$(".contacts__form-success").addClass("contacts__form-success--show")
-}
-});
-return false;
-});
-});
-
 successToggle.onclick = function() {
   successMessage.classList.remove('contacts__form-success--show');
-}
-
-
-let upButton = document.querySelector('.up-button');
+};
 
 window.onscroll = function () {
   if (window.pageYOffset > 700) {
     upButton.classList.add('up-button--show');
   } else {
     upButton.classList.remove('up-button--show');
-    }
-
+    };
 };
 
-let projectsItemDescriptions = document.querySelectorAll('.projects__item-description');
-let projectsItems = document.querySelectorAll('.projects__item');
+let clearSection = function () {
+  for (let projectsItemDescription of projectsItemDescriptions) {
+    projectsItemDescription.classList.remove('projects__item-description--show');
+  }
+};
+
 
 for (let projectsItem of projectsItems) {
   let itemDescription = projectsItem.querySelector('.projects__item-description');
   projectsItem.onclick = function (event) {
     event.stopPropagation();
-      itemDescription.classList.toggle('projects__item-description--show');
+    clearSection();
+    itemDescription.classList.toggle('projects__item-description--show');
   };
-};
-
-document.onclick = function (e) {
-  for (let projectsItemDescription of projectsItemDescriptions) {
-    
-      projectsItemDescription.classList.remove('projects__item-description--show');
-    
-  }
 }
+
+document.onclick = function () {
+  clearSection();
+};
